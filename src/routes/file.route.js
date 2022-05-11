bc69fd1ac4a9a3970adfcde2ccc7ca87839339e8
Saif Hossain <atonlymesaif@gmail.com>
2022-05-11 23:49:06 +0600

    file uploading response implemented

--- src/routes/file.route.js
+++ src/routes/file.route.js
@@ -8,9 +8,22 @@ const fileRoute = express.Router();
 const uploader = multer({ storage });
 
 fileRoute.post('/upload', uploader.single('file'), async (req, res) => {
-    console.log(req.file);
-
-    res.send('nice');
+    try {
+        res.status(201).send(
+            {
+                filename: req.file.filename,
+                contentType: req.file.contentType,
+                size: parseFloat(parseFloat(req.file.size)/(1024*1024)).toFixed(2) +'mb',
+                uploadDate: req.file.uploadDate,
+            }
+        );
+    } catch (e) {
+        console.log(e);
+        res.send({
+            error: true,
+            message: 'Error while uploading.',
+        });
+    }
 });
 
 module.exports = fileRoute;
