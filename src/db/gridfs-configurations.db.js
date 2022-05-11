a5b4b108e93fd1f5f23f49a9d0d29f53b47a2c4d
Saif Hossain <atonlymesaif@gmail.com>
2022-05-11 23:31:42 +0600

    chunk uploading done

--- src/db/gridfs-configurations.db.js
+++ src/db/gridfs-configurations.db.js
@@ -5,6 +5,7 @@ const mongodbURL = process.env.MONGODB_URL;
 const storage = new GridFsStorage({
     url: mongodbURL,
     file: async (req, file) => {
+        console.log(file);
         return {
             filename: 'file_' + Date.now(),
             bucketName: 'uploadedFiles',
--- src/routes/file.route.js
+++ src/routes/file.route.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const multer = require('multer');
+
+const storage = require('../db/gridfs-configurations.db');
 
 const fileRoute = express.Router();
+
+const uploader = multer({ storage });
+
+fileRoute.post('/upload', uploader.single('file'), async (req, res) => {
+    // console.log(req.file);
+
+    res.send('nice');
+});
 
 module.exports = fileRoute;
